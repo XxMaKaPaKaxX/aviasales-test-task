ac2026c3ff3466ed770d8b2de97dbe9568ff3025
Max Lyovin <lyovin.maxim@gmail.com>
2021-03-06 22:19:55 +0100

    realised filter by price or fly duration

--- src/modules/Tickets.jsx
+++ src/modules/Tickets.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import Ticket from '../components/Ticket';
 
-const Tickets = ({ ticketsPack, amountShowedTickets }) => {
+const Tickets = ({ ticketsPack, amountShowedTickets, filterCheaperOrFaster }) => {
 
-    const tickets = ticketsPack.slice(0, amountShowedTickets)
+
+
+    const filterByLowestPrice = (arr) => {
+        const sortedPack = arr.sort((a, b) => a.price - b.price);
+        return sortedPack;
+    }
+    const filterByAmountDuration = (arr) => {
+        const sortedPack = arr.sort((a, b) => (a.segments[0].duration + a.segments[1].duration) - (b.segments[0].duration + b.segments[1].duration));
+        return sortedPack;
+    }
+
+    const getFilteredByPriceOrTime = (arr) => {
+        switch (filterCheaperOrFaster) {
+            case 'cheapest':
+                return filterByLowestPrice(arr)
+            case 'fastest':
+                return filterByAmountDuration(arr)
+            default:
+                console.log('some wrong action')
+                break;
+        }
+    }
+
+    /* console.log(getFilteredByPriceOrTime(ticketsPack)) */
+
+    const filteredTickets = getFilteredByPriceOrTime(ticketsPack)
+
+    const tickets = filteredTickets.slice(0, amountShowedTickets)
         .map((ticket, index) => <Ticket key={index} data={ticket} />)
+
+
 
 
     return (
