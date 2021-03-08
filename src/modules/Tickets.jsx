7a5dc6a3fec0467ec016c8f0266ef68b90868805
Max Lyovin <lyovin.maxim@gmail.com>
2021-03-08 17:49:09 +0100

    added filter by transfers

--- src/modules/Tickets.jsx
+++ src/modules/Tickets.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import Ticket from '../components/Ticket';
 
-const Tickets = ({ ticketsPack, amountShowedTickets, filterCheaperOrFaster }) => {
-
-
+const Tickets = ({ ticketsPack, amountShowedTickets, filterCheaperOrFaster, transfersCheckboxesStatus }) => {
 
     const filterByLowestPrice = (arr) => {
         const sortedPack = arr.sort((a, b) => a.price - b.price);
@@ -26,14 +24,42 @@ const Tickets = ({ ticketsPack, amountShowedTickets, filterCheaperOrFaster }) =>
         }
     }
 
-    /* console.log(getFilteredByPriceOrTime(ticketsPack)) */
+    const prepareFiltermatrix = (obj) => {
+        const filterMatrix = [];
+        for (const option in obj) {
+            if (option === "all" && obj[option] === true) {
+                filterMatrix.push("all");
+            } else if (option === "without" && obj[option] === true) {
+                filterMatrix.push(0);
+            } else if (option === "one" && obj[option] === true) {
+                filterMatrix.push(1);
+            } else if (option === "two" && obj[option] === true) {
+                filterMatrix.push(2);
+            } else if (option === "three" && obj[option] === true) {
+                filterMatrix.push(3);
+            }
+        }
+        return filterMatrix;
+    }
 
-    const filteredTickets = getFilteredByPriceOrTime(ticketsPack)
+    const filterByTransfers = (arr) => {
+        const filterMatrix = prepareFiltermatrix(transfersCheckboxesStatus);
+        if (filterMatrix.includes('all')) {
+            return arr;
+        }
+
+        const result = arr.filter(ticket => {
+            const amountStopsTo = [...(ticket.segments[0].stops)].length   //wtf bez rest operatora length nie działa
+            const amountStopsFrom = [...(ticket.segments[1].stops)].length
+            return (filterMatrix.includes(amountStopsTo) && filterMatrix.includes(amountStopsFrom))
+        })
+        return result;
+    }
+
+    const filteredTickets = filterByTransfers(getFilteredByPriceOrTime(ticketsPack));
 
     const tickets = filteredTickets.slice(0, amountShowedTickets)
         .map((ticket, index) => <Ticket key={index} data={ticket} />)
-
-
 
 
     return (
